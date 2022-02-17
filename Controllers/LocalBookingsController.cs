17bc3f0b8145982a23addda9e37d5b02593e764c
Terese Holmlund <teho0003@student.hv.se>
2022-02-17 11:32:07 +0100

    two controllers

---
+++ Controllers/LocalBookingsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocalScheduleServiceAPI.Models;
+
+namespace LocalScheduleServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocalBookingsController : ControllerBase
+    {
+        private readonly LocalContext _context;
+
+        public LocalBookingsController(LocalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LocalBookings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LocalBooking>>> GetLocalBooking()
+        {
+            return await _context.LocalBooking.ToListAsync();
+        }
+
+        // GET: api/LocalBookings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LocalBooking>> GetLocalBooking(int id)
+        {
+            var localBooking = await _context.LocalBooking.FindAsync(id);
+
+            if (localBooking == null)
+            {
+                return NotFound();
+            }
+
+            return localBooking;
+        }
+
+        // PUT: api/LocalBookings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLocalBooking(int id, LocalBooking localBooking)
+        {
+            if (id != localBooking.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(localBooking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocalBookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LocalBookings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<LocalBooking>> PostLocalBooking(LocalBooking localBooking)
+        {
+            _context.LocalBooking.Add(localBooking);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocalBooking", new { id = localBooking.Id }, localBooking);
+        }
+
+        // DELETE: api/LocalBookings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLocalBooking(int id)
+        {
+            var localBooking = await _context.LocalBooking.FindAsync(id);
+            if (localBooking == null)
+            {
+                return NotFound();
+            }
+
+            _context.LocalBooking.Remove(localBooking);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LocalBookingExists(int id)
+        {
+            return _context.LocalBooking.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/LocalsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocalScheduleServiceAPI.Models;
+
+namespace LocalScheduleServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocalsController : ControllerBase
+    {
+        private readonly LocalContext _context;
+
+        public LocalsController(LocalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Locals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Local>>> GetLocals()
+        {
+            return await _context.Locals.ToListAsync();
+        }
+
+        // GET: api/Locals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Local>> GetLocal(int id)
+        {
+            var local = await _context.Locals.FindAsync(id);
+
+            if (local == null)
+            {
+                return NotFound();
+            }
+
+            return local;
+        }
+
+        // PUT: api/Locals/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLocal(int id, Local local)
+        {
+            if (id != local.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(local).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Locals
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Local>> PostLocal(Local local)
+        {
+            _context.Locals.Add(local);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocal", new { id = local.Id }, local);
+        }
+
+        // DELETE: api/Locals/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLocal(int id)
+        {
+            var local = await _context.Locals.FindAsync(id);
+            if (local == null)
+            {
+                return NotFound();
+            }
+
+            _context.Locals.Remove(local);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LocalExists(int id)
+        {
+            return _context.Locals.Any(e => e.Id == id);
+        }
+    }
+}
